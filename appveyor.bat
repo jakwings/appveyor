52b1cca468bad9a83b25120cc3a7768a3f0a7e84
Jak Wings <jakwings@gmail.com>
2016-03-25 21:41:32 +0800

    test 3

--- appveyor.bat
+++ appveyor.bat
@@ -1,3 +1,7 @@
-cd C:\Libraries\boost_1_59_0
-bootstrap
-b2 --prefix=%BOOST_ROOT% toolset=msvc-12.0 variant=release link=static threading=multi runtime-link=static --with-locale -q -d0 install
+if 1 == 1 (
+	cd C:\Libraries\boost_1_59_0
+	bootstrap
+)
+if 1 == 1 (
+	b2 --prefix=%BOOST_ROOT% toolset=msvc-12.0 variant=release link=static threading=multi runtime-link=static --with-locale -q -d0 install
+)
